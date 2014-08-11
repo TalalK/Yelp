54fb612192afe7fa3b4d897da327460741e0300f
Talal Khodr <Talal.Khodr@Gmail.com>
2014-08-11 17:32:05 +0100

    Added the Add, delete and edit restaurant options

--- spec/features/restaurant_feature_spec.rb
+++ spec/features/restaurant_feature_spec.rb
@@ -10,8 +10,42 @@ describe 'restaurants' do
 		end
 	end
 
+	context 'restaurants have been added' do
+		before do
+			Restaurant.create(name: 'Maze')
+		end
 
+		it 'should display them' do
+			visit '/restaurants'
+			expect(page).to have_content 'Maze'
+		end
+	end
 
+	context 'Users can add, delete and edit restaurants' do
 
+		before do
+			Restaurant.create(name: 'Maze')
+		end
+
+		it 'allows the user to add a restaurant' do
+			visit '/restaurants'
+			click_link 'Add a restaurant'
+			fill_in 'Name', with: 'Byron'
+			click_button 'Create Restaurant'
+			expect(page).to have_content 'Byron'
+		end
+		it 'Allows a user to edit a restaurant' do
+			visit '/restaurants'
+			click_link 'Edit Maze'
+			fill_in 'Name', with: 'Hakkasan'
+			click_button 'Update Restaurant'
+			expect(page).to have_content 'Hakkasan'
+		end
+		it 'Allows a user to delete a restaurant' do
+			visit '/restaurants'
+			click_link 'Delete Maze'
+			expect(page).not_to have_content 'Maze'
+		end
+	end
 
 end
